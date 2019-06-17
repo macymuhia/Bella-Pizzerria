bd3a9efff70573974409980b2deda208b03a035b
macymuhia <macymuhia@gmail.com>
2019-06-17 15:48:10 +0300

    Add jquery function to add selected pizza options to cart

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,11 @@ function Cart(name, size, crust, topping) {
     this.topping = topping;
 }
 
+function changeToNumber(elementId) {
+    inputToBeChanged = parseInt(getElementById(elementId).value);
+    return inputToBeChanged;
+}
+
 function pizzaChoice() {
     var pizzaName = document.getElementById("pizza").value;
     var pizzaSize = document.getElementById("size").value;
@@ -20,7 +25,15 @@ function pizzaChoice() {
     return myCartChoice;
 }
 
+// FRONT-END LOGIC
+
+function clickBtn() {
+
+    window.location.href = "#order";
+}
+
 $(document).ready(function() {
+
     //Preloader
     $(window).on("load", function() {
         preloaderFadeOutTime = 2000;
@@ -31,5 +44,19 @@ $(document).ready(function() {
         }
         hidePreloader();
     });
+
+    // Getting user choices    
+
+    $("#add-to-cart").click(function() {
+
+        var selectedPizza = document.getElementById("pizza").value;
+        var selectedSize = document.getElementById("size").value;
+        var selectedCrust = document.getElementById("crust").value;
+        var selectedTopping = document.getElementById("topping").value;
+        var quantity = document.getElementById("qty").value;
+
+        $(".modal-body ul").append("<li>" + quantity + " " + selectedSize + " " + selectedPizza + " Pizza " + " (" + selectedCrust + ", " + selectedTopping + ")" + "</li>");
+    });
+
 
 });
